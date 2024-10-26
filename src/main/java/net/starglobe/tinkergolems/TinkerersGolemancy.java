b9a0ce4bdd4329ad3a5a82066cc684cb067bb1b1
MarshMiloMoon <milomoon06@gmail.com>
2024-10-26 10:52:19 -0700

    initial setup and added the Wrench

--- src/main/java/net/starglobe/tinkergolems/TinkerersGolemancy.java
+++ src/main/java/net/starglobe/tinkergolems/TinkerersGolemancy.java
@@ -2,6 +2,7 @@ package net.starglobe.tinkergolems;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.starglobe.tinkergolems.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class TinkerersGolemancy implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/starglobe/tinkergolems/item/ModItems.java
@@ -0,0 +1,33 @@
+package net.starglobe.tinkergolems.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.starglobe.tinkergolems.TinkerersGolemancy;
+
+public class ModItems {
+    public static final Item WRENCH = registerItem("wrench", new SwordItem(ToolMaterials.IRON,
+            3, -2.4f, new FabricItemSettings()));
+
+    private static void addItemsToToolTab(FabricItemGroupEntries entries) {
+        entries.add(WRENCH);
+    }
+    private static void addItemsToCombatTab(FabricItemGroupEntries entries) {
+        entries.add(WRENCH);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TinkerersGolemancy.MOD_ID, name), item);
+    }
+
+    public static void  registerModItems() {
+        TinkerersGolemancy.LOGGER.info("Registering Mod Items for " + TinkerersGolemancy.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(ModItems::addItemsToToolTab);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ModItems::addItemsToCombatTab);
+    }
+}
