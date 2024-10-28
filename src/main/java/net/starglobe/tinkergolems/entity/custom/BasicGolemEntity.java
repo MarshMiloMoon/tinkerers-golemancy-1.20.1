3b684fd37fc5ddcdcd88b5945f04a13eae52a060
MarshMiloMoon <milomoon06@gmail.com>
2024-10-27 23:52:27 -0700

    made the basic golem have a diamond helment and shear to take it off and stuff

--- src/main/java/net/starglobe/tinkergolems/entity/custom/BasicGolemEntity.java
+++ src/main/java/net/starglobe/tinkergolems/entity/custom/BasicGolemEntity.java
@@ -13,11 +13,16 @@ import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.BeeEntity;
 import net.minecraft.entity.passive.GolemEntity;
 import net.minecraft.entity.passive.SnowGolemEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 
 public class BasicGolemEntity extends GolemEntity implements Shearable {
     private static final TrackedData<Byte> BASIC_GOLEM_FLAGS = DataTracker.registerData(BasicGolemEntity.class, TrackedDataHandlerRegistry.BYTE);
@@ -25,6 +30,12 @@ public class BasicGolemEntity extends GolemEntity implements Shearable {
 
     public BasicGolemEntity(EntityType<? extends GolemEntity> entityType, World world) {
         super(entityType, world);
+    }
+
+    @Override
+    protected void initDataTracker() {
+        super.initDataTracker();
+        this.dataTracker.startTracking(BASIC_GOLEM_FLAGS, (byte)16);
     }
 
     @Override
@@ -40,11 +51,56 @@ public class BasicGolemEntity extends GolemEntity implements Shearable {
     }
 
     @Override
+    public void writeCustomDataToNbt(NbtCompound nbt) {
+        super.writeCustomDataToNbt(nbt);
+        nbt.putBoolean("Hat", this.hasHat());
+    }
+
+    @Override
+    public void readCustomDataFromNbt(NbtCompound nbt) {
+        super.readCustomDataFromNbt(nbt);
+        if (nbt.contains("Hat")) {
+            this.setHasHat(nbt.getBoolean("Hat"));
+        }
+    }
+
+    @Override
+    protected ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        if (itemStack.isOf(Items.SHEARS) && this.isShearable()) {
+            this.sheared(SoundCategory.PLAYERS);
+            this.emitGameEvent(GameEvent.SHEAR, player);
+            if (!this.getWorld().isClient) {
+                itemStack.damage(1, player, playerx -> playerx.sendToolBreakStatus(hand));
+            }
+
+            return ActionResult.success(this.getWorld().isClient);
+        } else if (itemStack.isOf(Items.DIAMOND_HELMET) && !this.isShearable()) {
+            this.equiped(SoundCategory.PLAYERS);
+            this.emitGameEvent(GameEvent.EQUIP, player);
+            if (!this.getWorld().isClient) {
+                itemStack.damage(1, player, playerx -> playerx.sendToolBreakStatus(hand));
+            }
+
+            return ActionResult.success(this.getWorld().isClient);
+        } else {
+            return ActionResult.PASS;
+        }
+    }
+
+    @Override
     public void sheared(SoundCategory shearedSoundCategory) {
         this.getWorld().playSoundFromEntity(null, this, SoundEvents.ENTITY_SNOW_GOLEM_SHEAR, shearedSoundCategory, 1.0F, 1.0F);
         if (!this.getWorld().isClient()) {
             this.setHasHat(false);
             this.dropStack(new ItemStack(Items.DIAMOND_HELMET), 1.7F);
+        }
+    }
+
+    public void equiped(SoundCategory shearedSoundCategory) {
+        this.getWorld().playSoundFromEntity(null, this, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, shearedSoundCategory, 1.0F, 1.0F);
+        if (!this.getWorld().isClient()) {
+            this.setHasHat(true);
         }
     }
 
@@ -57,9 +113,9 @@ public class BasicGolemEntity extends GolemEntity implements Shearable {
         return (this.dataTracker.get(BASIC_GOLEM_FLAGS) & 16) != 0;
     }
 
-    public void setHasHat(boolean hasPumpkin) {
+    public void setHasHat(boolean hasHat) {
         byte b = this.dataTracker.get(BASIC_GOLEM_FLAGS);
-        if (hasPumpkin) {
+        if (hasHat) {
             this.dataTracker.set(BASIC_GOLEM_FLAGS, (byte)(b | 16));
         } else {
             this.dataTracker.set(BASIC_GOLEM_FLAGS, (byte)(b & -17));
